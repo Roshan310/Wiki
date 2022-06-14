788f21666c3566bb6a5d5389555561af064dc10e
roshan <yuvrajaryal83@gmail.com>
2022-06-14 11:40:58 +0545

    second commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn project_wiki.wsgi:application --log-file -
--- project_wiki/settings.py
+++ project_wiki/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9b%0h1!z@xeu%vwi)-3&+n$kj1*t-ve@(-#gaj8bc4sd=et!_(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -75,13 +75,15 @@ WSGI_APPLICATION = 'project_wiki.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'postgres',
+       'USER': 'postgres',
+       'PASSWORD': 'roshan123',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
